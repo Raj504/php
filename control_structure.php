19272f3708db565a936dfe0795ddeeddeb88e4c8
Raj <rajaryan8930@gmail.com>
2024-06-23 00:42:13 +0530

    created a control structure

--- control_structure.php
+++ control_structure.php
@@ -1,10 +1,19 @@
 
 <?php
-$x = 10;
+$number=60;
+if($number>60){
+    echo "first";
+} elseif($number>=45 && $number<=60){
+    echo "second";
+} else{
+    echo "third"; 
+}
+
+/*$x = 10;
 if($x==10){
     echo "yes";
 }
-/*
+
 =  initialize the value
 == do comparison
 === compare value and check datatype*/
